29ebae01c559a2c0d45d0d866fa32936dfc9d756
Zhihui Tang <crafttang@gmail.com>
2017-06-20 09:39:46 +0200

    add Logger

--- app/src/main/java/com/tang/myapplication/MainActivity.java
+++ app/src/main/java/com/tang/myapplication/MainActivity.java
@@ -2,11 +2,13 @@ package com.tang.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.tang.craftkit.log.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Logger.d("MainActivity onCreate");
     }
 }
---
+++ craftkit/src/main/java/com/tang/craftkit/log/Util.java
@@ -0,0 +1,9 @@
+package com.tang.craftkit.log;
+
+/**
+ * Created by zhaokaiqiang on 15/12/11.
+ */
+public class Util {
+
+
+}
